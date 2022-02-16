a355e85d177bd399efb2dd59dce71fea4cb07a26
athlona64 <manzer@manzers-MacBook-Pro.local>
2022-02-16 15:38:00 +0700

    faucet

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
 import json
 from web3 import Web3, HTTPProvider
 from web3.contract import ConciseContract
-from mnemonic import Mnemonic
-from coincurve import PrivateKey
-from bip44 import Wallet
-from bip44.utils import get_eth_addr
 from os import environ as env
 
 from dotenv import load_dotenv
 load_dotenv()
+abi = env['abi']
+bytecode = env['bytecode']
 
-
-mnemo = Mnemonic("english")
-
-# words = mnemo.generate(strength=256)
-# words = mnemo.generate();
-# print(words);
-
-
-
-w = Wallet(env['MNEMONIC'])
-sk, pk = w.derive_account("eth", account=0)
-# print(pk);
-sk = PrivateKey(sk)
-sk.public_key.format() == pk
-addr = get_eth_addr(pk)
-
-print(addr);
+# print(addr);
 # web3.py instance
-# apiKey = "xeXhUjdEEpBfAFuCDwRftiCr0xx_hHFy";
-# w3 = Web3(HTTPProvider("https://eth-kovan.alchemyapi.io/v2/" + apiKey))
-# print(w3.isConnected())
+w3 = Web3(HTTPProvider(env['RPC']))
+print(w3.isConnected())
 
-# key="<Private Key here with 0x prefix>"
+key=env['privateKey']
+acct = w3.eth.account.privateKeyToAccount(key)
+
+print(acct.address)
+print(w3.eth.get_balance(acct.address))
+transaction = {
+  'to': Web3.toChecksumAddress("0xd6539a16aaf14de06ad26731a0fec5242d505ee2"),
+  'from': Web3.toChecksumAddress(acct.address),
+  'value': 0,
+  'data':"0x3ccfd60b",
+  'nonce': w3.eth.get_transaction_count(acct.address),
+  'gas': 2000000,
+  'maxFeePerGas': 2000000000,
+  'maxPriorityFeePerGas': 1000000000,
+  'chainId': w3.eth.chain_id
+}
+signed = w3.eth.account.sign_transaction(transaction, key)
+signed.rawTransaction
+w3.eth.send_raw_transaction(signed.rawTransaction)
